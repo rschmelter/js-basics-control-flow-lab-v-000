6fb81d5772997b93e38e25b39ab55a7a3eea12a4
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-01 00:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,3 @@ function scuberGreetingForFeet (feet) {
   }
 }
 
-function ternaryCheckCity(city)
-  if (city === 'NYC') {
-    return 'Ok, sounds good.'
-    else {
-      return 'No go.'
-    }
-  }
