fbb950b64ede4c760b4ce09815bded270edf1ac2
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-01 00:44:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-let message
 
-if (feet <= 400) {
-  message = 'This one is on me!'
-  elsif (feet > 400)
-  message = 'I will gladly take your thirty bucks.'
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2000)
+    return 'I will gladly take your thirty bucks.'
+  } else {return 'No can do.'
   }
-  else
-  message = 'No can do.'
-  return message
 }
