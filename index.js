0db51ef1aff2db87456780f5a920e6bec273d265
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-01 00:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code in this file!
-function scuberGreetingForFeet(value)
+function scuberGreetingForFeet(value) {
 let messge
 
 if (scuberGreetingForFeet(value <= 400) {
   message = 'This one is on me!'
 
+}
 }
